2746fc590ca8bc5ad52ba906308cd825ae89a3c4
Liviu Maftuleac <liviu.maftuleac@gmail.com>
2017-03-17 14:18:50 +0200

    Added encapsulated Module declaration
    - lightNg('moduleName')
    
    Added possibility to include other modules
    
    - lightNg('moduleName').include('moduleName')
    
    This update is not backward compatible, an update of version is required

--- index.js
+++ index.js
@@ -1,133 +1,202 @@
+'use strict';
+
 (function(){
-    
 
-    function lightNg() {
-        var store = {};
-        var self = this;
-        var pending = [];
-        var running = false;
+    //modules collector        
+    var Modules = {};
 
-        //resolves injector
-        function getInjectables (injector) {
-            var injectables = [];
-            for (var i=0; i<injector.length-1; i++) {
-                if (typeof injector[i] === 'string') {
-                    if (store[injector[i]]) {
-                        injectables.push(store[injector[i]]);
-                    } else {
-                        throw "Unknown provider : '"+ injector[i] +"'"
-                    }
-
-                } else {
-                    throw "Unknown type in injector: "+injector[i];
-                }
-            }
-            return injectables;
-        }
-
-        //initiates a service instance
-        function initiateService(name, injector){
-            var injectables = getInjectables(injector),
-                constructorFn = injector.pop();
-
-            var ServiceInstance = function(args){
-                constructorFn.apply(this, args);
-                return this;
-            }
-
-            store[name] = new ServiceInstance(injectables);
-        }
-
-        //initiates a factory instance
-        function initiateFactory(name, injector) {
-            var injectables = getInjectables(injector);
-            var operation;
-            if (typeof injector === 'function') {
-                operation = injector;
-            } else {
-                operation = injector.pop();
-            }
-
-            store[name] = operation.apply(this, injectables);
-        }
-
-        //initiates a run instance
-        function initiateRunner(injector){
-            var injectables = getInjectables(injector);
-            var operation = injector.pop();
-            operation.apply(this,injectables);
-        }
-
-        /**
-         * PUBLIC 
-         */
-
-        this.service = function(name, injector) {
-            if (!running) {
-                pending.push({
-                    constructor : initiateService,
-                    arguments : [name,injector]
-                });
-            } else {
-                initiateService(name, injector);
-            }
-
-            return self;
-        }
-
-        this.factory = function(name, injector) {
-            if (!running) {
-                pending.push({
-                    constructor : initiateFactory,
-                    arguments : [name,injector]
-                })
-            } else {
-                initiateFactory(name, injector);
-            }
-            return self;
-        }
-
-        this.provider = function(name, constructor) {
-            if (typeof constructor !== "function") {
-                throw 'Provider constructor for "'+name+'" is not a function';
-            }
-            store[name] = constructor();
-            return self;
-        }
-
-        this.run = function(injector) {
-            if (!running) {
-                pending.push({
-                    constructor : initiateRunner,
-                    arguments : [injector]
-                });
-            } else {
-                initiateRunner(injector);
-            }
-            return self;
-        }
-
-        this.build = function(){
-            if(running) return;
-            running = true;
-            var component;
-            while (pending.length>0) {
-                component = pending.shift();
-                if (typeof component.constructor !== 'function') {
-                    throw 'Unknown constructor for "'+component.name+'"';
-                }
-                component.constructor.apply(self,component.arguments);
-            }
-        };
-
-        this.extend = function(name, exp){
-            self[name] = exp;
-            return self;
-        }
-        
-        this.noop = function(){}
-
+    function lightNgModuleConstructor() {
+        this.__lightNgModule = true;
+        this.store = {};
+        this.pending = [];
+        this.running = false;
     }
+
+    //resolves injector
+    lightNgModuleConstructor.prototype._getInjectables = function(injector) {
+        var _self = this;
+        var injectables = [];
+        for (var i=0; i<injector.length-1; i++) {
+            if (typeof injector[i] === 'string') {
+                if (_self.store[injector[i]]) {
+                    injectables.push(_self.store[injector[i]]);
+                } else {
+                    throw "Unknown provider : '"+ injector[i] +"'"
+                }
+
+            } else {
+                throw "Unknown type in injector: "+injector[i];
+            }
+        }
+        return injectables;
+    }
+
+    //initiates a service instance
+    lightNgModuleConstructor.prototype._initiateService = function(name, injector) {
+        var _self = this;
+        var injectables = _self._getInjectables(injector),
+            constructorFn = injector.pop();
+
+        var ServiceInstance = function(args){
+            constructorFn.apply(this, args);
+            return this;
+        }
+
+        _self.store[name] = new ServiceInstance(injectables);
+    }
+
+    //initiates a factory instance
+    lightNgModuleConstructor.prototype._initiateFactory = function(name, injector) {
+        var _self = this;
+        var injectables = _self._getInjectables(injector);
+        var operation;
+        if (typeof injector === 'function') {
+            operation = injector;
+        } else {
+            operation = injector.pop();
+        }
+
+        _self.store[name] = operation.apply(this, injectables);
+    }
+
+    //initiates a run instance
+    lightNgModuleConstructor.prototype._initiateRunner = function(injector) {
+        var _self = this;
+        var injectables = _self._getInjectables(injector);
+        var operation = injector.pop();
+        operation.apply(this,injectables);
+    }
+
+    /**
+     * PUBLIC 
+     */
+
+    lightNgModuleConstructor.prototype.service = function(name, injector) {
+        var _self = this;
+        if (!_self.running) {
+            _self.pending.push({
+                constructor : _self._initiateService,
+                arguments : [name,injector]
+            });
+        } else {
+            _.self._initiateService(name, injector);
+        }
+
+        return _self;
+    }
+
+    lightNgModuleConstructor.prototype.factory = function(name, injector) {
+        var _self = this;
+        if (!_self.running) {
+            _self.pending.push({
+                constructor : _self._initiateFactory,
+                arguments : [name,injector]
+            })
+        } else {
+            _self._initiateFactory(name, injector);
+        }
+        return _self;
+    }
+
+    lightNgModuleConstructor.prototype.provider = function(name, constructor) {
+        var _self = this;
+        if (typeof constructor !== "function") {
+            throw 'Provider constructor for "'+name+'" is not a function';
+        }
+        _self.store[name] = constructor();
+        return _self;
+    }
+
+    lightNgModuleConstructor.prototype.run = function(injector) {
+        var _self = this;
+        if (!_self.running) {
+            _self.pending.push({
+                constructor : _self._initiateRunner,
+                arguments : [injector]
+            });
+        } else {
+            _self._initiateRunner(injector);
+        }
+        return _self;
+    }
+
+    lightNgModuleConstructor.prototype.build = function(){
+        var _self = this;
+        if(_self.running) return;
+        _self.running = true;
+        var component;
+        while (_self.pending.length>0) {
+            component = _self.pending.shift();
+            if (typeof component.constructor !== 'function') {
+                throw 'Unknown constructor for "'+component.name+'"';
+            }
+            component.constructor.apply(_self,component.arguments);
+        }
+        return _self;
+    };
+
+    lightNgModuleConstructor.prototype.extend = function(name, exp){
+        var _self = this;
+        _self[name] = exp;
+        return _self;
+    }
+
+    lightNgModuleConstructor.prototype.include = function(moduleName) {
+        var _self = this;
+        
+        if (_self.running) {
+            throw 'Cannot load modules after .build()';
+        }
+
+        var module = Modules[moduleName];
+
+        if (typeof module !== 'object') {
+            throw 'Faild Loading module, unknow data type';
+        }
+
+        //include incoming module
+
+        var moduleStore = module.store;
+
+        for (var key in moduleStore) { 
+            if (moduleStore.hasOwnProperty(key)) {
+                if (_self.store[key]) {
+                    throw "'"+key+"' already exists, check namings and try again";
+                } else {
+                    _self.store[key] = moduleStore[key];
+                }
+            }
+        }
+
+        var modulePending = module.pending;
+
+        for (var i=0; i<modulePending.length; i++) {
+            _self.pending.push(modulePending[i]);
+        }
+
+        return _self;
+    }
+
+    //initiator
+    function lightNg(moduleName) {
+        if (!moduleName) {
+             throw 'lightNg moduleName Required';
+        }
+
+        var module = Modules[moduleName];
+
+        if (module) {
+            //return existing module
+            return module;
+        } else {
+            //create new module
+            module = new lightNgModuleConstructor();
+            Modules[moduleName] = module;
+            return module;
+        }
+    }
+
+    lightNg.noop = function(){}
 
         //check if browser or node
     if (typeof module !== 'undefined' && module.exports) {
